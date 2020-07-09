350952d99d5d414eff1c570806d374ddb49efc05
mohdhaider <mohdhaider1811@gmail.com>
2020-07-10 02:46:06 +0530

    Added default placeholder image for non image resrtaurennt.

--- SearchApp/food/src/screens/RestaurantCell.js
+++ SearchApp/food/src/screens/RestaurantCell.js
@@ -5,22 +5,32 @@ import { View, Text, Image, StyleSheet } from 'react-native'
 
 const RestaurantCell = ({ restaurant }) => {
 
-    console.log(restaurant)
+    //console.log(restaurant)
+
+    const urlImage = <Image style={styles.imageStyle} source={{ uri: restaurant.thumb }} />
+    const placeholderImage = <Image style={styles.imageStyle} source={require('../../assets/placeholder.jpg')} />
+    const ratingText = <Text style={styles.ratingStyle}>{restaurant.user_rating.rating_text}</Text>
 
     return (
         <View>
+            <Text></Text>
             <Text style={styles.titleStyle}>{restaurant.name}</Text>
             <Text></Text>
-            <Image style={styles.imageStyle}
-                source={{ uri: restaurant.thumb }}
-            />
+            {restaurant.thumb ? urlImage : placeholderImage}
+            <Text></Text>
+            {restaurant.user_rating.rating_text ? ratingText : null}
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     titleStyle: {
-        fontSize: 16
+        fontSize: 17,
+        fontWeight: 'bold'
+    },
+    ratingStyle: {
+        fontSize: 14,
+        fontWeight: '500'
     },
     imageStyle: {
         width: 250,
