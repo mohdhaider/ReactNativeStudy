5c32087058e1282c15d5118c1a179d61037372ea
mohdhaider <mohdhaider1811@gmail.com>
2020-07-20 00:48:02 +0530

    Added blog post context and provided.

---
+++ Blog/blog/src/screens/IndexScreen.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+import BlogContext from '../context/BlogContext'
+
+const IndexScreen = () => {
+
+    const blogPosts = useContext(BlogContext)
+
+    return (
+        <View>
+            <FlatList
+                data={blogPosts}
+                keyExtractor={(post) => post.title}
+                renderItem={({item}) => {
+                    return (
+                        <Text>Title: {item.title}</Text>
+                    )
+                }}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default IndexScreen;
